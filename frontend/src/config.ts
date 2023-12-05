a0b7259ac96146f769157012390d01ec78e2e4d3
Richard Wessels <richardwesselsofficial@gmail.com>
2023-12-05 21:37:08 +0100

    Revert to old URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,4 @@
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8001";
-const API_BASE_URL = "http://localhost:8001";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8001";
 export default {
     // URL of the RESTful API
     apiBaseUrl: `${API_BASE_URL}/api/v1`
