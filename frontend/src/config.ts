71aa00e5ed7989bdaae1bca28fc9033039c2375e
Richard Wessels <richardwesselsofficial@gmail.com>
2023-12-05 21:29:42 +0100

    Trying to change API_BASE_URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8001";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8001";
+const API_BASE_URL = "http://localhost:8001";
 export default {
     // URL of the RESTful API
     apiBaseUrl: `${API_BASE_URL}/api/v1`
