446a0ff1c078e5e28626e83bd91ccb337ea40986
Richard Wessels <richardwesselsofficial@gmail.com>
2023-12-07 11:49:12 +0100

    Added extra fields to registration

--- frontend/src/views/UserRegistrationView.vue
+++ frontend/src/views/UserRegistrationView.vue
@@ -3,34 +3,62 @@
 import { ref } from 'vue';
 import config from "@/config";
 
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
 const username = ref('')
 const password = ref('')
+const passwordConfirmation = ref('')
 const isDriver = ref(false)
 const licensePlateNum = ref('')
 
 
 function registerUser() {
+
+  let body = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    username: username.value,
+    password: password.value,
+  }
   if (isDriver) {
+    body.licensePlateNum = licensePlateNum.value
     fetch(`${config.apiBaseUrl}/addDriver`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: username.value,
-        password: password.value,
-        licensePlateNum: licensePlateNum.value
-      })
-    }).then(res=>console.log(res))
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      }).then(res=>console.log(res))
   }
   else {
     fetch(`${config.apiBaseUrl}/addRider`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: username.value,
-        password: password.value,
-      })
-    }).then(res=>console.log(res))
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      }).then(res=>console.log(res))
   }
+
+  // if (isDriver) {
+  //   fetch(`${config.apiBaseUrl}/addDriver`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       username: username.value,
+  //       password: password.value,
+  //       licensePlateNum: licensePlateNum.value
+  //     })
+  //   }).then(res=>console.log(res))
+  // }
+  // else {
+  //   fetch(`${config.apiBaseUrl}/addRider`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       username: username.value,
+  //       password: password.value,
+  //     })
+  //   }).then(res=>console.log(res))
+  // }
   
 }
 
@@ -40,13 +68,18 @@ function registerUser() {
   <main>
     <h1>User registration</h1>
     <form @submit.prevent="registerUser">
+    <input v-model="firstName" placeholder="Enter first name">
+    <input v-model="lastName" placeholder="Enter last name">
+    <input v-model="email" placeholder="Enter email">
     <input v-model="username" placeholder="Enter username">
     <input v-model="password" placeholder="Enter password" type="password">
+    <input v-if="password.length > 0" v-model="passwordConfirmation" placeholder="Confirm password" type="password">
     <br>
     <label><input type="checkbox" v-model="isDriver">Register as driver?</label>
     <br>
     <input v-if="isDriver" v-model="licensePlateNum" placeholder="License plate number">
     <br>
+    <!-- <button :disabled="password != confirmPassword">Register</button> -->
     <button>Register</button>
   </form>
   </main>
