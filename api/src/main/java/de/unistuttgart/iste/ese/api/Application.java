2476af8b1796c6eebab907b09095c77e5a033450
Robert <33040161+rtryan98@users.noreply.github.com>
2023-12-08 17:22:17 +0100

    Add further settings to make cors work with chrome

--- api/src/main/java/de/unistuttgart/iste/ese/api/Application.java
+++ api/src/main/java/de/unistuttgart/iste/ese/api/Application.java
@@ -24,7 +24,9 @@ public class Application {
                 // allow CORS requests for all resources and HTTP methods from the frontend origin
                 CorsRegistration registration = registry.addMapping("/**")
                     .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE")
-                    .allowedOrigins("http://localhost:5173");
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
                 String frontendUrl = System.getenv("FRONTEND_URL");
                 if (frontendUrl != null) {
                     registration.allowedOrigins(frontendUrl);
