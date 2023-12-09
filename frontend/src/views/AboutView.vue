dfe108126c85eda6809a566ae4b7f968ef75ee1d
Khouloud Trabelsi <khouloudtrabelsi@s-8d3ae033.off.site.uni-stuttgart.de>
2023-12-09 01:31:03 +0100

    About view added

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+    <h3>Welcome to "e.mundo's Ride & Share" – your ultimate companion through the dynamic world of commuting for Mundo society workers!</h3>
+    <h2>Our Mission</h2>
+    <h3>At "e.mundo's Ride & Share", we recognize the diverse journeys of e.mundo society workers. Navigating train delays, closures, and replacement services can be challenging for both groups. Our mission is to enhance the commuting experience, fostering connections and creating a sense of community during your travels.</h3>
+    <h2>Why "e.mundo's Ride & Share"</h2>
+<h3>Developed as part of a software internship, our app is designed to benefit e.mundo society workers. We believe that commuting is an opportunity for shared experiences and meaningful connections.</h3>
+<h2>Be a Part of Our Journey</h2>
+<h3>Join "e.mundo's Ride & Share" and embark on a new way of commuting. Strengthen the network, share insights, and make your journey a collaborative adventure.</h3>
   </div>
 </template>
+<style scoped>
+
+
+h1 {
+  font-family: 'Arial', sans-serif;
+  font-weight: bold; 
+  text-align: center;
+  margin-bottom: 20px; /* Adjust the margin as needed */
+
+}
+h3 {
+  
+  text-align: center;
+  margin-top: 30px; /* Adjust the margin as needed */
+
+}
+h2 {
+  font-family: 'Arial', sans-serif;
+  font-style: italic;
+  font-size: 24px;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  text-align: center;
+  text-decoration: underline;
+  margin-top: 30px; /* Adjust the margin as needed */
+
+}
+</style>
