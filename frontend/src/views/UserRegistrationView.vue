8a6087882eb4bb854b5e22719b8a36e7d9c33919
Richard Wessels <richardwesselsofficial@gmail.com>
2023-12-09 14:05:05 +0100

    Added password + password confirmation check to user registration

--- frontend/src/views/UserRegistrationView.vue
+++ frontend/src/views/UserRegistrationView.vue
@@ -18,23 +18,28 @@ const router = useRouter()
 
 
 function registerUser() {
+  if (password.value != passwordConfirmation.value) {
+    showToast(new Toast("Registration failed", "Ensure all fields are filled in correctly"))
+    return false
+  }
 
     fetch(`${config.apiBaseUrl}/users`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value, // temporary
-      description: "default description",
-      carDescription: "default car description",
-     })
-  })
-  .then(res=>{
-      if (res.status != 201) {throw "Registration failed"}
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value, // temporary
+        password: password.value,
+        description: "default description",
+        carDescription: "default car description",
+      })
     })
-  .then(res=>router.push({ path: '/' }))
-  .catch(err=>showToast(new Toast("Registration failed", "Ensure all fields are filled in correctly")))
+    .then(res=>{
+        if (res.status != 201) {throw "Registration failed"}
+      })
+    .then(res=>router.push({ path: '/' }))
+    .catch(err=>showToast(new Toast("Registration failed", "Ensure all fields are filled in correctly")))
 
 }
 
@@ -50,6 +55,7 @@ function registerUser() {
     <input v-model="username" placeholder="Enter username">
     <input v-model="password" placeholder="Enter password" type="password">
     <input v-if="password.length > 0" v-model="passwordConfirmation" placeholder="Confirm password" type="password">
+
     <br>
     <label><input type="checkbox" v-model="isDriver">Register as driver?</label>
     <br>
